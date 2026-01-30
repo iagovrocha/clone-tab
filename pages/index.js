fba6e2d7556ef93b9f31565bb46e84658eef5d2a
Iago V. R. <iagovieira3562@gmail.com>
2026-01-29 23:11:39 -0300

    adiciona nova frase a pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import * as React from 'react';
+
 function Home() {
-    return <h1>Olá, Mundo!</h1>
+    const [state, setState] = React.useState(0);
+
+    function handleClick(st) {
+        setState(st)
+    }
+
+    return <>
+        <h2>Frase filosófica </h2>
+        <h1>{state == 0 ?
+        '"A violência é o último refúgio dos incompetentes" - Salvor Hardin em "Fundação"' :
+        "'Ele criou o daltonismo'- Saas 2020"
+        }</h1>
+        <button onClick={() => handleClick(state == 0 ? 1 : 0)}> Clique aqui para ver a verdadeira frase filosófica </button>
+    </>
 }
 
 export default Home;
